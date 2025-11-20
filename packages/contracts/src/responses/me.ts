161bbf7bff4ddf713bec698de0477ab88b1894a4
Max Anstey <maxanstey@gn.apc.org>
2025-11-20 21:11:35 +0000

    refactor: rename apps to api and ui

--- packages/contracts/src/responses/me.ts
+++ packages/contracts/src/responses/me.ts
@@ -6,8 +6,6 @@ export type MeResponse = {
   user: User;
 };
 
-// TODO: Document that explicit typing = MeResponse/User type is exported
-//       e.g. cmd click await client.auth.me and you'll see
 export const meResponseSchema: z.ZodType<MeResponse> = z.object({
   user: userSchema,
 });
